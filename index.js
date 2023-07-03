0b027903dcafcbf5c9662b3c583e15c7d5954a86
Christelle Bs <christelle.lbs34@gmail.com>
2023-07-03 21:08:06 +0200

    envoi 1

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// const getColor = () => {
+// const r = Math.floor(Math.random() * 255);
+// const g = Math.floor(Math.random() * 255);
+// const b = Math.floor(Math.random() * 255);
+
+// const color = `rgb(${r}, ${g}, ${b})`
+// console.log(r,g,b);
+
+// document.body.style.background = color;
+// document.body.innerHTML = `<h2>${color}</h2>`;
+// }
+// setInterval(getColor, 1000);
+
+// -------- ou -----------
+
+function getColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+  
+    const color = `rgb(${r}, ${g}, ${b})`;
+  
+    document.body.style.background = color;
+    document.body.innerHTML = `<h2>${color}</h2>`;
+  }
+  
+  setInterval(getColor, 1000);
+  
